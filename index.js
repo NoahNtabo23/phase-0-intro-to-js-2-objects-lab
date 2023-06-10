13fb0bc25d8233bdb2d1cda2c1170ade1a3b6ba6
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-11 01:03:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: "Noah",
+    streetAddress: 545-23568,
+  };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]:value,
+    }
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employee1 = {
+        ...employee1,
+        key: key
+    }
+    return employee1
+} 
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    employee[key] = undefined;
+    return employee;
+}
